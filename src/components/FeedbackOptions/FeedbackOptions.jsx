f50fa6e991cf442d6cba55f822c6816f428e0c5c
MaksymSkala <maksymskala@gmail.com>
2023-10-10 22:59:18 +0300

    Add FeedbackOptions.jsx

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const FeedbackOptions = ({ onLeaveFeedback }) => {
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return (
     <div>
-      <button className="button-class" onClick={() => onLeaveFeedback('good')}>Good</button>
-      <button className="button-class" onClick={() => onLeaveFeedback('neutral')}>Neutral</button>
-      <button className="button-class" onClick={() => onLeaveFeedback('bad')}>Bad</button>
+      {options.map((option) => (
+        <button key={option.type} className="button-class" onClick={() => onLeaveFeedback(option.type)}>
+          {option.label}
+        </button>
+      ))}
     </div>
   );
 };
 
 FeedbackOptions.propTypes = {
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      type: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+    })
+  ).isRequired,
   onLeaveFeedback: PropTypes.func.isRequired,
 };
 
